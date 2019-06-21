f81f0eb43a8f1366f202796d7679fc06353a8ac2
seanacres <6416690+seanacres@users.noreply.github.com>
2019-06-21 11:25:53 -0400

    Set up outlets

---
+++ Pokedex/Pokedex/PokemonDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PokemonDetailViewController.swift
+//  Pokedex
+//
+//  Created by Sean Acres on 6/21/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var pokemonName: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pokemonID: UILabel!
+    @IBOutlet weak var pokemonTypes: UILabel!
+    @IBOutlet weak var pokemonAbilities: UILabel!
+    @IBOutlet weak var savePokemonButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func savePokemonButtonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
