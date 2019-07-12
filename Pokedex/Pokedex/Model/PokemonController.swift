971388411371f0bb948374447a4e029d12a24cb8
seanacres <6416690+seanacres@users.noreply.github.com>
2019-07-12 11:55:33 -0400

    Added pokemon search function

---
+++ Pokedex/Pokedex/Model/PokemonController.swift
@@ -0,0 +1,71 @@
+//
+//  PokemonController.swift
+//  Pokedex
+//
+//  Created by Sean Acres on 7/12/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import Foundation
+
+class PokemonController {
+    enum NetworkError: Error {
+        case otherError
+        case badData
+        case noDecode
+        case noEncode
+        case badResponse
+        case badAuth
+        case noAuth
+    }
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case put = "PUT"
+        case post = "POST"
+        case delete = "DELETE"
+    }
+    
+    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    var savedPokemon: [Pokemon] = []
+    
+    // search by name or id
+    func searchForPokemon(searchTerm: String, completion: @escaping (Result<Pokemon, NetworkError>) -> Void) {
+        let requestURL = baseURL.appendingPathComponent(searchTerm)
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            // check for error
+            if let error = error {
+                NSLog("error searching: \(error)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            // check for response code
+            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                NSLog("Bad response while searching: \(response)")
+                completion(.failure(.badResponse))
+                return
+            }
+            
+            // check for data
+            guard let data = data else {
+                NSLog("No data returned from search")
+                completion(.failure(.badData))
+                return
+            }
+            
+            // decode data and pass to result
+            let jsonDecoder = JSONDecoder()
+            do {
+                let pokemon = try jsonDecoder.decode(Pokemon.self, from: data)
+                completion(.success(pokemon))
+            } catch {
+                NSLog("failed to decode from search: \(error)")
+                completion(.failure(.noDecode))
+            }
+        }.resume()
+    }
+}
